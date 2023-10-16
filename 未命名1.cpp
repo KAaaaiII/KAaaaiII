6e4c4b46b43f8bec534fac86c002707271fa9772
KAaaaiII <1870308096@qq.com>
2023-10-16 19:12:50 +0800

    Update 未命名1.cpp

--- 未命名1.cpp
+++ 未命名1.cpp
@@ -3,31 +3,7 @@
 using namespace std;
 int main()
 {
-	int i, score;
-	cout << "你考试考了多少分?(0-100):";
-	cin >> score;
-	if (score > 100 || score < 0)
-		cout << "分数值必须在0-100之间！";
-	else
-	{
-		i = score / 10;
-		switch (i)
-		{
-		case10:
-		case9:
-			cout << "你的成绩为优！";
-			break;
-		case8:
-
-			cout << "你的成绩为良！";
-			break;
-		case6:
-		case7:
-			cout << "你的成绩为中！";
-			break;
-		default:
-			cout << "你的成绩为差！";
-		}
+	
 		return 0;
 	}
 }
